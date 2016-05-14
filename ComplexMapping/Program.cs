70d5c3ab6ffa4887d0fbd37aede021de1b632d0a
luke.chen <xiaoyong6906@126.com>
2016-05-14 13:18:45 +0800

    [Research] Demoes of TinyMapper, coding by VS2013

---
+++ ComplexMapping/Program.cs
@@ -0,0 +1,43 @@
+﻿using Nelibur.ObjectMapper;
+using System;
+using System.Collections.Generic;
+
+namespace ComplexMapping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TinyMapper.Bind<PersonComplex, PersonDtoComplex>(config => {
+                config.Ignore(x => x.CreateTime);
+                config.Ignore(x => x.Nickname);
+                config.Bind(x => x.FirstName, y => y.FirstName);//order property name
+            });
+
+            var person = new PersonComplex
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Luke",
+                LastName = "Chen",
+                Address = new Address
+                {
+                    Phone = "XXXX",
+                    Street = "IT Street",
+                    ZipCode = "424600"
+                },
+                CreateTime = DateTime.Now,
+                Nickname = "Yong",
+                Emails = new List<string> { 
+                    "xiaoyong6906@126.com",
+                    "xiaoyong6907@126.com"
+                }                      
+            };
+
+            var personDto = TinyMapper.Map<PersonDtoComplex>(person);
+            Console.WriteLine(personDto.Nickname == null);
+            Console.WriteLine(personDto.FirstName);
+            Console.ReadLine();
+        }                               
+    }
+}
---
+++ TinyMapperDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nelibur.ObjectMapper;
+using Nelibur.ObjectMapper.Bindings;
+
+namespace SimpleMapping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //First of all Bind Person type to PersonDto type and we don't want map Email property. So it has been ignored.
+            TinyMapper.Bind<Person, PersonDto>(config => {
+                config.Ignore(x => x.Email);
+            });
+
+            var person = new Person { 
+                Id = Guid.NewGuid(),
+                FirstName = "Luke",
+                LastName = "Chen",
+                Email = "xiaoyong6906@126.com"
+            };
+
+            //Now TinyMapper knows how to map Person object to PersonDto object. Finally, call
+            PersonDto personDto = TinyMapper.Map<PersonDto>(person);
+            Console.WriteLine("personDto:" + personDto.Id + personDto.FirstName + personDto.LastName + personDto.Email);
+            Console.ReadLine();
+        }
+    }
+}
